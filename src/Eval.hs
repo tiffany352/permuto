4ae9b8efb9ab7433f60b1f7b2df320547bd3145e
Tiffany Bennett <tiffany@stormbit.net>
2015-02-21 01:57:33 -0500

    Definitions

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,4 +1,4 @@
-module Eval (push, pop, Context, Stack, evalExpr, evalExprs, unquote, number) where
+module Eval (push, pop, Context(..), Stack, evalExpr, evalExprs, unquote, number) where
 
 import Data.Map as Map
 import Data.Maybe
@@ -8,21 +8,36 @@ push v st = v:st
 
 pop (v:st) = (v, st)
 
-type Context = Map [Char] (Stack -> Result Stack)
+newtype Context = Context { funcs :: Map [Char] (Context -> Stack -> Result Stack) }
 
 type Stack = [Expr]
 
-evalExpr :: Context -> Stack -> Expr -> Result Stack
-evalExpr c st e = case e of
-                  Atom a -> case Map.lookup a c of
-                              Just x -> x st
-                              Nothing -> Left $ "No such atom '"++a++"'"
-                  Quote q -> Right $ push (Quote q) st
-                  v -> Right $ push (Quote [v]) st
+wrapFunc v c s = fmap (\(s',_) -> s') $ evalExprs c s v
 
-evalExprs :: Context -> Stack -> [Expr] -> Result Stack
-evalExprs c st (e:es) = evalExpr c st e >>= (\st' -> evalExprs c st' es)
-evalExprs c st [] = Right st
+libDef :: Context -> Stack -> Result (Stack, Context)
+libDef c (n:Quote v:st) =
+    case string n of
+      Just n' ->
+          Right (st, Context $ Map.insert n'
+                 (wrapFunc v) (funcs c))
+      Nothing -> Left "Right parameter of def must be string"
+libDef c (n:v:st) = Left "Left parameter of def must be quoted"
+libDef c _ = Left "def requires two parameters"
+
+evalExpr :: Context -> Stack -> Expr -> Result (Stack, Context)
+evalExpr c st e =
+    case e of
+      Atom "def" ->
+          libDef c st
+      Atom a -> case Map.lookup a (funcs c) of
+                  Just x -> fmap (\st' -> (st', c)) $ x c st
+                  Nothing -> Left $ "No such atom '"++a++"'"
+      Quote q -> Right (push (Quote q) st, c)
+      v -> Right (push (Quote [v]) st, c)
+
+evalExprs :: Context -> Stack -> [Expr] -> Result (Stack, Context)
+evalExprs c st (e:es) = evalExpr c st e >>= (\(st',c') -> evalExprs c' st' es)
+evalExprs c st [] = Right (st, c)
 
 unquote :: Expr -> [Expr]
 unquote (Quote q) = q
@@ -32,3 +47,8 @@ number :: Expr -> Maybe Int
 number (Quote [Number e]) = Just e
 number (Number e) = Just e
 number _ = Nothing
+
+string :: Expr -> Maybe [Char]
+string (Quote [String s]) = Just s
+string (String s) = Just s
+string _ = Nothing
--- src/main.hs
+++ src/main.hs
@@ -9,51 +9,54 @@ read s = case readExprs s of
            Right (es, []) -> Right es
            Left x -> Left x
 
-print :: Result [Expr] -> [Char]
-print (Right st) = Lexer.showExprs . reverse $ st
+print :: Result ([Expr], Context) -> [Char]
+print (Right (st,_)) = Lexer.showExprs . reverse $ st
 print (Left e) = e
 
 once c s = Main.print $ Main.read s >>= evalExprs c []
 
 loop c = do
   line <- getLine
-  putStrLn $ once c line
-  loop c
+  let r = Main.read line >>= evalExprs c []
+  putStrLn $ Main.print r
+  case r of
+    Right (_, c') -> loop c'
+    _ -> loop c
 
-libAdd (a:b:st) =
+libAdd c (a:b:st) =
     maybeResult "Expected number"
               (Monad.liftM2 (+) (number a) (number b)
                >>= Just . (flip push st) . Number)
-libAdd _ = Left "add requires two parameters"
-libShow (a:st) = Right $ push (String $ show a) st
-libShow _ = Left "show requires a parameter"
-libI c (Quote q:st) = evalExprs c st q
+libAdd c _ = Left "add requires two parameters"
+libShow c (a:st) = Right $ push (String $ show a) st
+libShow c _ = Left "show requires a parameter"
+libI c (Quote q:st) = fmap (\(st', c)-> st') $ evalExprs c st q
 libI c _ = Left "Quoted parameter required"
-libZap (v:st) = Right st
-libZap _ = Left "Can't zap an empty stack"
-libDup (v:st) = Right $ v:v:st
-libDup _ = Left "Can't dup an empty stack"
-libSwap (a:b:st) = Right $ b:a:st
-libSwap _ = Left "swap requires two parameters"
-libDip c (Quote q:b:st) = fmap (push b) $ evalExprs c st q
+libZap c (v:st) = Right st
+libZap c _ = Left "Can't zap an empty stack"
+libDup c (v:st) = Right $ v:v:st
+libDup c _ = Left "Can't dup an empty stack"
+libSwap c (a:b:st) = Right $ b:a:st
+libSwap c _ = Left "swap requires two parameters"
+libDip c (Quote q:b:st) = fmap (\(st',c)-> push b st') $ evalExprs c st q
 libDip c (Quote q:st) = Left "dip requires two parameters"
 libDip c _ = Left "First argument to dip must be quoted"
-libUnit (a:st) = Right $ Quote [a] : st
-libUnit _ = Left "Can't unit empty stack"
-libCat (a:b:st) = Right $ Quote (unquote b ++ unquote a):st
-libCat _ = Left "cat requires two parameters"
-libCons (a:b:st) = Right $ Quote (b:unquote a):st
-libCons _ = Left "cons requires two parameters"
+libUnit c (a:st) = Right $ Quote [a] : st
+libUnit c _ = Left "Can't unit empty stack"
+libCat c (a:b:st) = Right $ Quote (unquote b ++ unquote a):st
+libCat c _ = Left "cat requires two parameters"
+libCons c (a:b:st) = Right $ Quote (b:unquote a):st
+libCons c _ = Left "cons requires two parameters"
 
 main = do
-  let c = Map.fromList [
+  let c = Context $ Map.fromList [
            ("+", libAdd),
            ("show", libShow),
-           ("i", libI c),
+           ("i", libI),
            ("zap", libZap),
            ("dup", libDup),
            ("swap", libSwap),
-           ("dip", libDip c),
+           ("dip", libDip),
            ("unit", libUnit),
            ("cat", libCat),
            ("cons", libCons)
