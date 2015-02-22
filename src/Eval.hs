e3986ec8fe1bce829580ee143b97eb35810eaa57
Tiffany Bennett <tiffany@stormbit.net>
2015-02-22 00:13:09 -0500

    Clean up some functions

--- src/Eval.hs
+++ src/Eval.hs
@@ -25,15 +25,15 @@ libDef c (n:v:st) = Left "Left parameter of def must be quoted"
 libDef c _ = Left "def requires two parameters"
 
 evalExpr :: Context -> Stack -> Expr -> Result (Stack, Context)
-evalExpr c st e =
-    case e of
-      Atom "def" ->
-          libDef c st
-      Atom a -> case Map.lookup a (funcs c) of
-                  Just x -> fmap (\st' -> (st', c)) $ x c st
-                  Nothing -> Left $ "No such atom '"++a++"'"
-      Quote q -> Right (push (Quote q) st, c)
-      v -> Right (push (Quote [v]) st, c)
+evalExpr c st (Atom "def") = libDef c st
+evalExpr c st (Atom a) =
+    case Map.lookup a (funcs c) of
+      Just x -> fmap (\st' -> (st', c)) $ x c st
+      Nothing -> Left $ "No such atom '"++a++"'"
+evalExpr c st (Quote q) =
+    Right (push (Quote q) st, c)
+evalExpr c st v =
+    Right (push (Quote [v]) st, c)
 
 evalExprs :: Context -> Stack -> [Expr] -> Result (Stack, Context)
 evalExprs c st (e:es) = evalExpr c st e >>= (\(st',c') -> evalExprs c' st' es)
--- src/Lexer.hs
+++ src/Lexer.hs
@@ -12,10 +12,8 @@ eof :: Result a
 eof = Left "Unexpected EOF"
 
 indexOf :: Char -> [Char] -> Maybe Int
-indexOf c (x:xs) =
-    if c == x
-    then Just 0
-    else fmap ((+)1) $ indexOf c xs
+indexOf c (x:xs) | c == x = Just 0
+indexOf c (x:xs) = fmap ((+)1) $ indexOf c xs
 indexOf c [] = Nothing
 
 readNumberContent :: Int -> [Char] -> Result (Int, [Char])
@@ -26,51 +24,40 @@ readNumberContent acc (c:s) =
 readNumberContent acc [] = Right (acc, [])
 
 readNumberStart :: [Char] -> Result (Int, [Char])
-readNumberStart (c:s) =
-    if isDigit c
-    then readNumberContent 0 (c:s)
-    else Left "Expected digit"
+readNumberStart (c:s) | isDigit c = readNumberContent 0 (c:s)
+readNumberStart (c:s) = Left "Expected digit"
 readNumberStart [] = eof
 
 readNumber :: [Char] -> Result (Int, [Char])
-readNumber (c:s) =
-    case c of
-      '-' -> fmap (\(n,t) -> (-n,t)) $ readNumberStart s
-      '+' -> readNumberStart s
-      _ -> readNumberStart (c:s)
-readNumber [] = eof
+readNumber ('-':s) =
+    fmap (\(n,t) -> (-n,t)) $ readNumberStart s
+readNumber ('+':s) = readNumberStart s
+readNumber s = readNumberStart s
 
-oneOf c (x:xs) =
-    if c == x
-    then True
-    else oneOf c xs
-oneOf c [] =
-    False
+oneOf c (x:xs) | c == x = True
+oneOf c (x:xs) = oneOf c xs
+oneOf c [] = False
+
+isIdent c = isAlpha c || oneOf c "`~!@#$%^&*()-_=+,<.>/?|"
 
 readAtomTail :: [Char] -> Result ([Char], [Char])
-readAtomTail (c:s) =
-    if isAlphaNum c || oneOf c "`~!@#$%^&*()-_=+,<.>/?|"
-    then fmap (\(xs, t) -> (c:xs, t)) $ readAtomTail s
-    else Right ([], c:s)
-readAtomTail [] = Right ([], [])
+readAtomTail (c:s) | isIdent c || isDigit c =
+    fmap (\(xs, t) -> (c:xs, t)) $ readAtomTail s
+readAtomTail s = Right ([], s)
 
 readAtom :: [Char] -> Result ([Char], [Char])
-readAtom (c:s) =
-    if isAlpha c || oneOf c "`~!@#$%^&*()-_=+,<.>/?|"
-    then fmap (\(xs, t) -> (c:xs, t)) $ readAtomTail s
-    else Left "Expected identifier start"
+readAtom (c:s) | isIdent c =
+    fmap (\(xs, t) -> (c:xs, t)) $ readAtomTail s
+readAtom (c:s) = Left "Expected identifier start"
 readAtom [] = eof
 
 readStringInternal :: [Char] -> [Char] -> Result ([Char], [Char])
-readStringInternal acc (c:s) =
-    case c of
-      '"' -> Right (acc, s)
-      _ -> readStringInternal (acc ++ [c]) s
+readStringInternal acc ('"':s) = Right (acc, s)
+readStringInternal acc (c:s) = readStringInternal (acc++[c]) s
 readStringInternal acc [] = eof
 
 readString :: [Char] -> Result ([Char], [Char])
-readString (c:s) | c == '"' =
-    readStringInternal "" s
+readString (c:s) | c == '"' = readStringInternal "" s
 readString _ = Left "Expected string start"
 
 readClosureInternal :: [Expr] -> [Char] -> Result ([Expr], [Char])
@@ -81,13 +68,11 @@ readClosureInternal es s =
       xs -> readExpr xs >>= \(e, t) -> readClosureInternal (es++[e]) t
 
 readClosure :: [Char] -> Result ([Expr], [Char])
-readClosure (c:s) | c == '[' =
-    readClosureInternal [] s
+readClosure (c:s) | c == '[' = readClosureInternal [] s
 readClosure _ = Left "Expected ["
 
 readQuote :: [Char] -> Result (Expr, [Char])
-readQuote (c:s) | c == '\'' =
-    readExpr s
+readQuote (c:s) | c == '\'' = readExpr s
 readQuote _ = Left "Expected '"
 
 data Expr = Number Int | Atom [Char] | String [Char] | Quote [Expr]
@@ -106,9 +91,8 @@ showExprs (e:es) = foldl (\a-> \b-> a ++ " " ++ (show b)) (show e) es
 showExprs [] = ""
 
 orElse :: Result a -> Result a -> Result a
-orElse a b = case a of
-               Right x -> Right x
-               Left _ -> b
+orElse (Right a) _ = Right a
+orElse (Left _) b = b
 
 readExpr :: [Char] -> Result (Expr, [Char])
 readExpr s =
