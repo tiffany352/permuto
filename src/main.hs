6682036168bef7cba32a90dae838dab4407addf5
Tiffany Bennett <tiffany@stormbit.net>
2015-04-11 19:14:45 -0400

    Handle whitespace in the REPL better

--- src/main.hs
+++ src/main.hs
@@ -16,24 +16,32 @@ read s = case readExprs s of
 putStrLnNotEmpty [] = return ()
 putStrLnNotEmpty s = putStrLn s
 
-loop c acc = do
-  putStr $ if acc /= []
-           then ">> "
-           else "> "
+hasBlock (c:s) | c == ':' = readSep s == [] || hasBlock s
+hasBlock (c:s) = hasBlock s
+hasBlock [] = False
+
+indent n = "\n" ++ replicate (n*2) ' '
+
+loop c acc i = do
+  putStr $ replicate (i+1) '>' ++ " "
   GHC.IO.Handle.hFlush GHC.IO.Handle.FD.stdout
   line <- getLine
-  let acc' = (acc ++ line)
+  let acc' = if acc == []
+             then line
+             else acc ++ indent i ++ line
   let r = Main.read acc' >>= evalExprs c []
   case (line, r) of
     ("quit", _) -> return ()
-    (' ':_, _) -> loop c $ acc' ++ "\n"
+    ([], _) -> loop c acc (max 0 $ i-1)
+    (x, _) | hasBlock x -> loop c acc' (i+1)
+    (_, _) | i > 0 -> loop c acc' i
     (_, Right (x, c')) -> do
                    putStrLnNotEmpty $ Lexer.showExprs x
-                   loop c' []
-    (_, Left EOF) -> loop c $ acc' ++ "\n"
+                   loop c' [] 0
+    (_, Left EOF) -> putStr "EOF" >> loop c acc' (i+1)
     (_, Left x) -> do
                    putStrLnNotEmpty $ show x
-                   loop c []
+                   loop c [] 0
 
 runFile c f = do
   contents <- readFile f
@@ -75,5 +83,5 @@ main = do
            ("dip", libDip)
           ]
   if Prelude.null args
-  then loop c []
+  then loop c [] 0
   else Prelude.foldl (>>) (return ()) $ fmap (runFile c) args
